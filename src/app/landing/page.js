58fe2a90d390984b0956c7c50fe2dfaba6aef7c4
Rhulani Ndhlovu <rndhlovu@defaults-Mac-mini-2.local>
2025-06-27 01:12:21 +0200

    update

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -12,7 +12,7 @@ export default function LandingTestPage() {
 
   const startCamera = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const stream = await navigator.mediaDevices.getUserMedia({ video: false });
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
       }
